363b5b1d033983d792e3f5691b4a4698da44105e
bradleyjsimons <brad@velveteenaudio.com>
2014-02-13 16:09:32 -0700

    Lab 6

--- lonelyTwitter/src/ca/ualberta/cs/lonelytwitter/IntentReaderActivity.java
+++ lonelyTwitter/src/ca/ualberta/cs/lonelytwitter/IntentReaderActivity.java
@@ -7,39 +7,49 @@ import android.content.Intent;
 
 public class IntentReaderActivity extends Activity {
 
-	public static final String TEXT_KEY = "TEXT";
-	public static final String TRANSFORM_KEY = "TRANSFORM";
+    public static final String TEXT_KEY = "TEXT";
+    public static final String TRANSFORM_KEY = "TRANSFORM";
 
-	public static final int NORMAL = 1;
-	public static final int REVERSE = 2;
-	public static final int DOUBLE = 3;
-	
-	private String text;
-	private int mode;
-	
-	public String getText() {
-		return text;
-	}
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_intent_reader);
-	}
-	
-	public String transformText(String text) {
-		switch (mode) {
-			case REVERSE:
-				char[] string =  text.toCharArray();
-				for (int i = 0; i < string.length / 2; i++) {
-					char tmp = string[i];
-					string[i] = string[string.length - i -1];
-					string[string.length - i - 1] = tmp;
-				}
-				return new String(string);
-			case DOUBLE:
-				return text + text;
-		}
-		return text;
-	}
+    public static final int NORMAL = 1;
+    public static final int REVERSE = 2;
+    public static final int DOUBLE = 3;
+
+    private String text;
+    private int mode;
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_reader);
+        
+        Intent intent = getIntent();
+        mode = intent.getIntExtra(TRANSFORM_KEY, NORMAL);
+        if (intent.getStringExtra(TEXT_KEY) != null) {
+        	text = transformText(intent.getStringExtra(TEXT_KEY));
+        } else {
+        	text = "default";
+        }
+        
+        ((TextView)findViewById(R.id.intentText)).setText(text);
+    }
+
+    public String transformText(String text) {
+        switch (mode) {
+            case REVERSE:
+                char[] string =  text.toCharArray();
+                for (int i = 0; i < string.length / 2; i++) {
+                    char tmp = string[i];
+                    string[i] = string[string.length - i -1];
+                    string[string.length - i - 1] = tmp;
+                }
+                return new String(string);
+            case DOUBLE:
+                return text + text;
+        }
+        return text;
+    }
 }
